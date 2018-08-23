5016afdaa2a966955a91339d8df55fd699317c10
RebeccaHough <rebecca.c.hough@blueyonder.co.uk>
2018-08-24 00:27:58 +0100

    update: dropdown now opens on click on mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,62 @@
 $('document').ready(function() {
-    //make dropdown open on hover
-    $('body').on('mouseenter mouseleave', '.dropdown', function (e) {
+
+    /** Inital dropdown setup */
+    setupDropdown();
+
+    /**
+     * Register event listener to change how dropdown opens upon window resize
+     */
+    $(window).resize(setupDropdown);
+
+    /**
+     * Choose how to open dropdown menus with class '.dropdown-link', depending on screen size
+     */
+    function setupDropdown() {
+        //if mobile menu is showing and .dropdown-link is set
+        if($('#mainNavToggler').css('display') != 'none' && $('.dropdown-link').attr('href') != '') {
+            //disable link to 'services.html'
+            $('.dropdown-link').removeAttr('href');
+            $('.dropdown-link').css('cursor', 'pointer');
+            //set dropdown to open on click
+            addDropdownClickEventHandler();
+        } else if($('.dropdown-link').attr('href') != 'services.html') {
+            //enable link to 'services.html'
+            $('.dropdown-link').attr('href', 'services.html');
+            //remove inline styling (set 'cursor' back to default)
+            $('.dropdown-link').removeAttr('style');
+            //set dropdown to open on hover
+            addDropdownHoverEventHandler();
+        } else {
+            addDropdownHoverEventHandler();
+        }
+    }
+
+    /**
+     * Remove dropdown click event handler and add dropdown hover event handler
+     */ 
+    function addDropdownHoverEventHandler() {
+        //if dropdown on click exists, remove it
+        $('body').off('click', '.dropdown', dropdownClickEventHandler);
+
+        //make dropdown open on hover
+        $('body').on('mouseenter mouseleave', '.dropdown', dropdownHoverEventHandler);
+    }
+
+    /**
+     * Remove dropdown click event handler and add dropdown hover event handler
+     */
+    function addDropdownClickEventHandler() {
+        //if dropdown on hover exists, remove it
+        $('body').off('mouseenter mouseleave', '.dropdown', dropdownHoverEventHandler);
+
+        //make dropdown open on click
+        $('body').on('click', '.dropdown', dropdownClickEventHandler);
+    }
+
+    /**
+     * Dropdown hover event handler function
+     */
+    function dropdownHoverEventHandler(e) {
         var dropdown = $(e.target).closest('.dropdown');
         var menu = $('.dropdown-menu', dropdown);
         dropdown.addClass('show');
@@ -9,7 +65,22 @@ $('document').ready(function() {
             dropdown[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
             menu[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
         });
-    });
+    }
+
+    /**
+     * Dropdown click event handler function
+     */
+    function dropdownClickEventHandler(e) {
+        var dropdown = $(e.target).closest('.dropdown');
+        var menu = $('.dropdown-menu', dropdown);
+        if(dropdown.hasClass('show')) {
+            dropdown.removeClass('show');
+            menu.removeClass('show');
+        } else {
+            dropdown.addClass('show');
+            menu.addClass('show');
+        }
+    }
 
     //parallax scroll header
     // $('.my-parallax-window').parallax({
@@ -21,19 +92,19 @@ $('document').ready(function() {
     // get: 'user',
     //     userId: '8383337896',
     //     accessToken: 'YOUR_ACCESS_TOKEN'
-    var feed = new Instafeed({
-        //clientId: 'YOUR_CLIENT_ID',
-        get: 'user',
-        userId: '8383337896',
-        target: 'instagramFeed',
-        sortBy: 'most-recent',
-        resolution: 'standard_resolution',
-        links: true,
-        after: (function() {
-            //TODO remove placeholder content
-        })
-    });
-    feed.run();
+    // var feed = new Instafeed({
+    //     //clientId: 'YOUR_CLIENT_ID',
+    //     get: 'user',
+    //     userId: '8383337896',
+    //     target: 'instagramFeed',
+    //     sortBy: 'most-recent',
+    //     resolution: 'standard_resolution',
+    //     links: true,
+    //     after: (function() {
+    //         //TODO remove placeholder content
+    //     })
+    // });
+    // feed.run();
 
     /*
         var loadButton = document.getElementById('load-more');
