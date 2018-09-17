35188c08b814ccf2614dc3b753568eb9114694b5
ElliottWaterman <elliott@aquaman.plus.com>
2018-09-17 16:49:55 +0100

    update: Services page modal prices, drop/box shadow

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -92,64 +92,68 @@ $('document').ready(function() {
     // });
 
     /* Instagram feed (won't work from 2020+) */
-    var imgCount = 0;
-    var feed = new Instafeed({
-        get: 'user',
-        userId: '8383337896',
-        accessToken: '8383337896.1677ed0.0afd711d80ef49f7950ea697b2843e11',
-        target: 'instagramFeed',
-        sortBy: 'most-recent',
-        resolution: 'standard_resolution',
-        links: true,
-        // Every 4 images it creates a row
-        filter: function(image) {
-            if (imgCount % 4 === 0 || imgCount === 0) {
-                image.customTagOpen = '<div class="row">';
-                image.customTagClose = '';
-            } else if ((imgCount + 1) % 4 === 0) {
-                image.customTagOpen = '';
-                image.customTagClose = '</div>';
-            } else {
-                image.customTagOpen = '';
-                image.customTagClose = '';
+    // If element #instagramFeed exists
+    if($('#instagramFeed').length) {
+        var imgCount = 0;
+        var feed = new Instafeed({
+            get: 'user',
+            userId: '8383337896',
+            accessToken: '8383337896.1677ed0.0afd711d80ef49f7950ea697b2843e11',
+            target: 'instagramFeed',
+            sortBy: 'most-recent',
+            resolution: 'standard_resolution',
+            links: true,
+            // Every 4 images it creates a row
+            filter: function(image) {
+                if (imgCount % 4 === 0 || imgCount === 0) {
+                    image.customTagOpen = '<div class="row">';
+                    image.customTagClose = '';
+                } else if ((imgCount + 1) % 4 === 0) {
+                    image.customTagOpen = '';
+                    image.customTagClose = '</div>';
+                } else {
+                    image.customTagOpen = '';
+                    image.customTagClose = '';
+                }
+                imgCount += 1;
+                return true;
+            },
+            template: `
+                {{model.customTagOpen}}
+                <div class="col-md insta-img">
+                    <a 
+                        target="_blank"
+                        href="{{image}}" 
+                        title="
+                            <p style='padding-right: 10px; margin-bottom: 0'>
+                             {{caption}}
+                            </p>
+                            <a href='{{link}}' style='float: right'>
+                                View on Instagram &nbsp;<span class='fas fa-external-link-alt fa-sm'></span>&nbsp;&nbsp;
+                            </a>"
+                    >
+                        <img src="{{image}}" alt="{{caption}}" class="img-fluid shadow-box"/>
+                    </a>
+                </div>
+                {{model.customTagClose}}
+            `,
+            after: (function() {
+                //TODO remove placeholder content
+            })
+        });
+        feed.run();
+    
+        /* Magnific Popup */
+        /* Creates a single gallery with all elements that have class "gallery" */
+        $('.gallery').magnificPopup({
+            type: 'image',
+            delegate: 'a',
+            gallery: {
+                enabled: true
             }
-            imgCount += 1;
-            return true;
-        },
-        template: `
-            {{model.customTagOpen}}
-            <div class="col-lg-3 insta-img">
-                <a 
-                    target="_blank"
-                    href="{{image}}" 
-                    title="
-                        <p style='padding-right: 10px; margin-bottom: 0'>
-                         {{caption}}
-                        </p>
-                        <a href='{{link}}' style='float: right'>
-                            View on Instagram &nbsp;<span class='fas fa-external-link-alt fa-sm'></span>&nbsp;&nbsp;
-                        </a>"
-                >
-                    <img src="{{image}}" alt="{{caption}}" class="img-fluid"/>
-                </a>
-            </div>
-            {{model.customTagClose}}
-        `,
-        after: (function() {
-            //TODO remove placeholder content
-        })
-    });
-    feed.run();
+        });
+    }
 
-    /* Magnific Popup */
-    /* Creates a single gallery with all elements that have class "gallery" */
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
 
     /*
     Instagram feed pagination
