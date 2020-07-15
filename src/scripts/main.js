aa27dff867e5d43a1ac021550fe22d89824e0fb8
RebeccaHough <rebecca.c.hough@blueyonder.co.uk>
2020-07-15 20:38:28 +0100

    fix: instagram gallery; add: loader and error message for insta gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -87,90 +87,84 @@ $('document').ready(function() {
         });
     }
 
-    /* Instagram feed (won't work from 2020+) */
-    // If element #instagramFeed exists
+    /* Instagram feed */
+    // If element #instagramFeed exists (i.e. if on gallery page)
     if($('#instagramFeed').length) {
         // Get instagram access token (refreshes every 60 days)
-        let accessTokenInsta = //'';
-        'IGQVJXVFhQNGtQN2lKeC1SY29RTGlEcnZAzaGdLLXJyaU1nRVZAKZAG52ZAV9uZA3dVZA2ppTFZAKZAnlYVXQxZAlBwVFVScFhEMzBuSklyMXdHNlU4cnlJemIxQ0duZAEtBcnZADRFRFanBWRHBn';
-        
-        // TODO: Get instagram access token from: https://www.instant-tokens.com/instagram/authorisations
-        /*fetch('https://ig.instant-tokens.com/users/f7255527-d9d7-4220-b0e1-f85c132e4aeb/instagram/17841400927660656/token?userSecret=p657qprubgnma6uo6rfmo', {mode: 'no-cors'})
-        .then(response => response.json())
-        .then(data => {
-            accessTokenInsta = data;
-            console.log(data);
-        })*/
-
+        let accessTokenInsta = '';
         $.ajax({
             type: 'get',
             dataType: 'json',
-            url: 'https://ig.instant-tokens.com/users/f7255527-d9d7-4220-b0e1-f85c132e4aeb/instagram/17841400927660656/token?userSecret=p657qprubgnma6uo6rfmo',
-
+            // Use proxy to bypass CORS on ig.instant-tokens.com
+            url: 'https://cors-anywhere.herokuapp.com/https://ig.instant-tokens.com/users/f7255527-d9d7-4220-b0e1-f85c132e4aeb/instagram/17841400927660656/token?userSecret=p657qprubgnma6uo6rfmo',
             success: function (response) {
-                console.log(response);
                 accessTokenInsta = response.Token;
+                createGallery()
+            },
+            error: function(xhr, status, error) {
+                // Display error message if image fetching fails
+                $('#gallery').css('display', 'none');
+                $('#galleryError').css('display', 'flex');
             }
         });
+
+        function createGallery() {
+            var imgCount = 0;
+            var feed = new Instafeed({
+                //debug: true,
+                accessToken: accessTokenInsta,
+                target: 'instagramFeed',
+                limit: 16,
+                transform: function(image) {
+                    // Every 4 images, create a row
+                    if (imgCount % 4 === 0 || imgCount === 0) {
+                        image.model.customTagOpen = '<div class="row">';
+                        image.model.customTagClose = '';
+                    } else if ((imgCount + 1) % 4 === 0) {
+                        image.model.customTagOpen = '';
+                        image.model.customTagClose = '</div>';
+                    } else {
+                        image.model.customTagOpen = '';
+                        image.model.customTagClose = '';
+                    }
+                    imgCount += 1;
+                    return image;
+                },
+                template: `
+                    {{model.customTagOpen}}
+                    <div class="col-md insta-img">
+                        <a 
+                            target="_blank"
+                            href="{{image}}" 
+                            title="{{caption}}"
+                            data-style="
+                                <p style='padding-right: 10px; margin-bottom: 0'>
+                                    {{caption}}
+                                </p>
+                                <a href='{{link}}' style='float: right'>
+                                    View on Instagram &nbsp;<span class='fas fa-external-link-alt fa-sm'></span>&nbsp;&nbsp;
+                                </a>"
+                        >
+                            <img src="{{image}}" alt="{{caption}}" class="img-fluid shadow-box"/>
+                        </a>
+                    </div>
+                    {{model.customTagClose}}
+                `
+            });
+            feed.run();
         
-        var imgCount = 0;
-        var feed = new Instafeed({
-            //debug: true,
-            accessToken: accessTokenInsta,
-            target: 'instagramFeed',
-            //sort: () => {},   // Function. Defaults to most recent
-            transform: function(image) {
-                // Every 4 images it creates a row
-                if (imgCount % 4 === 0 || imgCount === 0) {
-                    image.model.customTagOpen = '<div class="row">';
-                    image.model.customTagClose = '';
-                } else if ((imgCount + 1) % 4 === 0) {
-                    image.model.customTagOpen = '';
-                    image.model.customTagClose = '</div>';
-                } else {
-                    image.model.customTagOpen = '';
-                    image.model.customTagClose = '';
+            /* Magnific Popup */
+            /* Creates a single gallery with all elements that have class "gallery" */
+            $('.gallery').magnificPopup({
+                type: 'image',
+                delegate: 'a',
+                gallery: {
+                    enabled: true
+                },
+                image: {
+                    titleSrc: 'data-style'
                 }
-                imgCount += 1;
-                return image;
-            },
-            template: `
-                {{model.customTagOpen}}
-                <div class="col-md insta-img">
-                    <a 
-                        target="_blank"
-                        href="{{image}}" 
-                        title="{{caption}}"
-                        data-style="
-                            <p style='padding-right: 10px; margin-bottom: 0'>
-                                {{caption}}
-                            </p>
-                            <a href='{{link}}' style='float: right'>
-                                View on Instagram &nbsp;<span class='fas fa-external-link-alt fa-sm'></span>&nbsp;&nbsp;
-                            </a>"
-                    >
-                        <img src="{{image}}" alt="{{caption}}" class="img-fluid shadow-box"/>
-                    </a>
-                </div>
-                {{model.customTagClose}}
-            `,
-            after: (function() {
-                //TODO remove placeholder content
-            })
-        });
-        feed.run();
-    
-        /* Magnific Popup */
-        /* Creates a single gallery with all elements that have class "gallery" */
-        $('.gallery').magnificPopup({
-            type: 'image',
-            delegate: 'a',
-            gallery: {
-                enabled: true
-            },
-            image: {
-                titleSrc: 'data-style'
-            }
-        });
+            });
+        }
     }
 });
